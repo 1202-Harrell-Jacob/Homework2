7c150a1bac6e3cddebe2a1fbbee18c993415acb4
1202-Harrell-Jacob <112525488+1202-Harrell-Jacob@users.noreply.github.com>
2022-09-09 17:22:54 -0700

    Add files via upload

--- vari.c
+++ vari.c
@@ -5,10 +5,13 @@
 #include <stdio.h>
 
 int main(){
+	//varialbes
 	char first_test, second_test, third_test;
+	//setting variables equal
 	first_test = 'A';
 	second_test = 'B';
 	third_test = 'A'; 
+	//print statements
 	printf("First test: %c \n", first_test);
 	printf("Second test: %c \n", second_test);
 	printf("Third test: %c \n", third_test);
